e14c0d48c8768072d8c326f041f46b3815f4daa0
markantoine1 <markantoine1718@gmail.com>
2022-10-21 13:51:18 -0300

    add Bad exception solution

--- exception/src/application/Program.java
+++ exception/src/application/Program.java
@@ -34,18 +34,13 @@ public class Program {
             System.out.print("Check-out date (dd/MM/yyyy):");
             checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if (checkIn.before(now) || (checkOut.before(now))) {
-                System.out.println("Error in reservation: Reservation dates must be future dates");
-            } else if (!checkOut.after(checkIn)) {
-                System.out.println("Error in reservation: Check-out date must be after check-in date");
-            } else {
-                reservation.updateDates(checkIn, checkOut);
+            String error = reservation.updateDates(checkIn, checkOut);
+            if (error != null) {
+                System.out.println("Error in reservatio: " + error);
+            }else{
                 System.out.println(reservation);
             }
-        }
-
+            }
         sc.close();
     }
-
 }
